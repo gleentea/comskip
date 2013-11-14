7a782f1ec901c31f754dce3b3123ed36672171d9
hiroyuki nagata <newserver002@gmail.com>
2013-11-14 23:16:06 +0900

    Compile comskip.c with Linux.

--- comskip.h
+++ comskip.h
@@ -5,3 +5,20 @@
 #endif
 
 #define _UNICODE
+
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#ifdef __GNUC__
+   /* gcc */
+   typedef long long __int64;
+#else
+   /* msvc or clang */
+   typedef signed __int64 int64_t;
+   typedef unsigned __int64 uint64_t;
+#endif
