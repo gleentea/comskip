130b95043e499043d3edf6b6fa191abc2ea70020
Turenar <snswinhaiku.lo@gmail.com>
2014-01-14 23:10:22 +0900

    comskip_wrapper.sh: Quote vars

--- misc/comskip_wrapper.sh
+++ misc/comskip_wrapper.sh
@@ -12,12 +12,12 @@ if test $# -ne 2; then
     exit 1
 fi
 
-if ! test -f $1; then
+if ! test -f "$1"; then
     echo ".ini file $1 does not exists."
     exit 1
 fi
 
-if ! test -f $2; then
+if ! test -f "$2"; then
     echo "TS file $2 does not exists."
     exit 1
 fi
@@ -25,14 +25,14 @@ fi
 COMSKIP=/usr/local/bin/comskip
 FFMPEG=ffmpeg
 OPTIONS=--csvout
-INIFILE=$1
-TS_FILE=$2
+INIFILE="$1"
+TS_FILE="$2"
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib 
-${COMSKIP} ${OPTIONS} --ini=${INIFILE} ${TS_FILE}
+${COMSKIP} ${OPTIONS} --ini="${INIFILE}" "${TS_FILE}"
 
 FEXT="${TS_FILE##*.}"
-FILE_NAME=`basename ${TS_FILE} ${FEXT}`
+FILE_NAME="$(basename "${TS_FILE}" "${FEXT}")"
 
 if ! test -f "`pwd`/${FILE_NAME}vdr"; then
     echo ".vdr file does not exists...exit" 1>&2
@@ -55,7 +55,7 @@ BEGIN_TIME_ARRAY=()
 # main knitting end time array
 _END__TIME_ARRAY=()
 
-for LINE in `cat ${VDR_FILE} | awk {'print $1'}`;
+for LINE in `cat "${VDR_FILE}" | awk {'print $1'}`;
 do
     if test ${LINE_NO} = 1; then
         # skip first line
@@ -87,7 +87,7 @@ for _END__TIME in ${_END__TIME_ARRAY[@]}; do
     let i++
     FILE_PARTS=${i}
     echo "${FFMPEG} -y -i ${TS_FILE} -c copy -ss ${_END__TIME} -t ${BEGIN_TIME} -sn `pwd`/${FILE_NAME}-${FILE_PARTS}.ts"
-    CUT_FILE_LIST+=(`pwd`/${FILE_NAME}-${FILE_PARTS}.ts)
+    CUT_FILE_LIST+=(`pwd`/"${FILE_NAME}-${FILE_PARTS}.ts")
 
     # convert time from 1970-01-01 00:00:00(UNIX Time) 
     DATE_YMD=`date '+%Y/%m/%d'`
@@ -95,9 +95,11 @@ for _END__TIME in ${_END__TIME_ARRAY[@]}; do
     TIME__END_=`date -d "${DATE_YMD} ${_END__TIME}" "+%s"`
     DIFF_SEC=`expr ${TIME__END_} - ${TIME_BEGIN}`
     PLAY_TIME=`echo | awk -v D=${DIFF_SEC} '{printf "%02d:%02d:%02d",D/(60*60),D%(60*60)/60,D%60}'`
+    #mkfifo "$(pwd)/${FILE_NAME}-${FILE_PARTS}.ts"
     # ffmpeg -i <input_data> -ss <start_sec> -t <play_time> <output_data>
     echo "${FFMPEG} -y -i ${TS_FILE} -c copy -ss ${BEGIN_TIME} -t ${PLAY_TIME} -sn `pwd`/${FILE_NAME}-${FILE_PARTS}.ts"
-    ${FFMPEG} -y -i ${TS_FILE} -c copy -ss ${BEGIN_TIME} -t ${PLAY_TIME} -sn `pwd`/${FILE_NAME}-${FILE_PARTS}.ts
+    # if you use with mkfifo, run FFMPEG command IN BACKGROUND (just append &)
+    ${FFMPEG} -y -i "${TS_FILE}" -c copy -ss ${BEGIN_TIME} -t ${PLAY_TIME} -sn `pwd`/"${FILE_NAME}-${FILE_PARTS}.ts"
 done
 
 #
@@ -107,12 +109,12 @@ FFMPEG_CONCAT_STR="concat:"
 i=0
 for FILE in ${CUT_FILE_LIST[@]}; do
     if test ${i} != 0; then
-        FFMPEG_CONCAT_STR=${FFMPEG_CONCAT_STR}"|"
+        FFMPEG_CONCAT_STR="${FFMPEG_CONCAT_STR}|"
     fi
-    FFMPEG_CONCAT_STR=${FFMPEG_CONCAT_STR}${CUT_FILE_LIST[${i}]}
+    FFMPEG_CONCAT_STR="${FFMPEG_CONCAT_STR}${CUT_FILE_LIST[${i}]}"
     let i++
 done
 
 OUTPUT_FILE="`pwd`/CUT-${FILE_NAME}ts"
 echo "${FFMPEG} -i \"${FFMPEG_CONCAT_STR}\" -c copy ${OUTPUT_FILE}"
-${FFMPEG} -i "${FFMPEG_CONCAT_STR}" -c copy ${OUTPUT_FILE}
+${FFMPEG} -i "${FFMPEG_CONCAT_STR}" -c copy "${OUTPUT_FILE}"
