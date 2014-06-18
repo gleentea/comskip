97486203539f41a1771f9c0dcd1ca63057547ed6
gleentea <gleentea@gmail.com>
2014-06-19 03:24:41 +0900

    add settings.c/settings.h LoadSettings function move to setting.c

--- comskip.h
+++ comskip.h
@@ -4,15 +4,18 @@
 #define SUBVERSION "056-TYA Edition"
 #endif
 
+#include <limits.h>
+#include <sys/stat.h>
+
 #define _UNICODE
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif /** autoconf generated config.h */
 
-#define bool			int
-#define true			1
-#define false			0
+#define bool            int
+#define true            1
+#define false           0
 
 // define Sleep
 #ifndef _WIN32
@@ -43,4 +46,38 @@ typedef __int64 LONG_PTR;
 #else
 typedef long LONG_PTR;
 #endif
+
+#ifdef _WIN32
+typedef struct _stati64 __stat;
+#else
+typedef struct stat __stat;
+#endif
+
+FILE*           myfopen(const char * f, char * m);
+int             mystat(char * f, __stat * s);
+int             myremove(char * f);
+
+
+#ifdef _WIN32
+#if defined(WIN32) && !defined(__MINGW32__) && !defined(__MINGW64__)
+#define MAX_PATH _MAX_PATH
+#else  // MSVC
+#define MAX_PATH FILENAME_MAX
+#endif // MinGW32,64
+#elif  __unix__
+#define MAX_PATH _POSIX_PATH_MAX
+#else // Linux/BSD/MacOSX
+#error "MAX_PATH is undefined"
+#endif
+
+// Define detection methods
+#define BLACK_FRAME         1
+#define LOGO                2
+#define SCENE_CHANGE        4
+#define RESOLUTION_CHANGE   8
+#define CC                 16
+#define AR                 32
+#define SILENCE            64
+#define CUTSCENE          128
+
 #endif
---
+++ settings.h
@@ -0,0 +1,7 @@
+
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
+
+FILE* LoadSettings(int argc, char** argv);
+
+#endif
